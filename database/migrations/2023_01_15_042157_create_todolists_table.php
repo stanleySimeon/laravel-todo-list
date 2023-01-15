5dfa67814d419b05fde349d1f5a1788034f990bc
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-01-15 13:02:48 -0500

    Update

---
+++ database/migrations/2023_01_15_042157_create_todolists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Facade\Ignition\Tabs\Tab;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodolistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todolists', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('isCompleted')->default('false');
+            $table->string('todo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolists');
+    }
+}
